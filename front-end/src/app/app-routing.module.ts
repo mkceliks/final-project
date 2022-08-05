c1401a037fc163f60c768e186ffbdcd037100a05
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-08-05 23:27:20 +0300

    some routing updates

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { CartComponent } from './components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ProductComponent},
+  {path:"add-product",component:AddProductComponent},
+  {path:"cart",component:CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
