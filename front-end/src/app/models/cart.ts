cfa11c58b90a77135f6ed8360c52b284cd1c6c0a
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-08-05 23:27:04 +0300

    product and cart model created

---
+++ front-end/src/app/models/cart.ts
@@ -0,0 +1,7 @@
+export interface Cart {
+    ID:number;
+    ProductID:number;
+    Quantity:number;
+    Price:number;
+    Discount:number;
+  }
---
+++ front-end/src/app/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    ID: number;
+    Name: string;
+    Description: string;
+    Price: number;
+    Discount: number;
+    Tax: number;
+  }
