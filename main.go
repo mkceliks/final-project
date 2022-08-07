4e61425534a6a96431556927a470c01cbb65e3e2
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-08-07 20:28:03 +0300

    order and customer APIs generated

--- main.go
+++ main.go
@@ -17,14 +17,19 @@ func main() {
 
 	r.HandleFunc("/products", service.GetProducts).Methods("GET")               // GET all products
 	r.HandleFunc("/cart", service.GetCartItems).Methods("GET")                  // GET cart items
+	r.HandleFunc("/customers", service.GetCustomers).Methods("GET")             // GET all customers
 	r.HandleFunc("/getProductById/{id}", service.GetProductById).Methods("GET") // GET product by id
 
-	r.HandleFunc("/addProduct", service.InsertProduct).Methods("POST")               // POST a new product to the database. Requires a JSON body with the product details ( Quantity ).
+	r.HandleFunc("/addProduct", service.InsertProduct).Methods("POST")   // POST a new product to the database. Requires a JSON body with the product details ( Quantity ).
+	r.HandleFunc("/addCustomer", service.InsertCustomer).Methods("POST") // POST a new customer to the database. Requires a JSON body with the customer details ( UserName ).
+
 	r.HandleFunc("/addToCart/{id}", service.AddToCart).Methods("POST")               // add to cart
 	r.HandleFunc("/addOneItemToCart/{id}", service.AddOneItemToCart).Methods("POST") // POST one item to cart
 
 	r.HandleFunc("/deleteOneItemFromCart/{id}", service.DeleteOneItemFromCart).Methods("DELETE") // DELETE one item from cart
 	r.HandleFunc("/deleteAllCart/{id}", service.DeleteCartItem).Methods("DELETE")                // DELETE all cart items
+
+	r.HandleFunc("/order", service.Order).Methods("POST") // POST purchase order
 
 	handler := cors.AllowAll().Handler(r)
 	fmt.Printf("Server is running on port 8080")
